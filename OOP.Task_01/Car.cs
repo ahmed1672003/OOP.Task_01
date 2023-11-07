b7ed727477e8ac06643a7e57df1c1f7b118d79cb
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2023-11-07 12:32:11 +0200

    Add project files.

---
+++ OOP.Task_01/Car.cs
@@ -0,0 +1,56 @@
+﻿namespace OOP.Task_01;
+internal sealed class Car
+{
+    // Id
+    // Plate (letters,numbers)
+    // LicenseExpirationDate
+    // LicenseCreationDate
+    // IsLicenseValid
+    // Model
+    // Color
+    // Speed
+    // Price
+    private string letters;
+    private string numbers;
+    internal int Id { get; set; }
+    internal string Letters
+    {
+        set { letters = value; }
+        private get { return letters; }
+    }
+    internal string Numbers
+    {
+        set { numbers = value; }
+        private get { return numbers; }
+    }
+    internal string Plate
+    {
+        get
+        {
+            return $"[{Letters},{Numbers}]";
+        }
+    }
+    internal DateTime LicenseExpirationDate
+    {
+        get
+        {
+            return LicenseCreationDate.AddYears(10);
+        }
+    }
+    internal bool IsLicenseValid
+    {
+        get
+        {
+            return DateTime.Now < LicenseExpirationDate;
+        }
+    }
+    internal string Model { get; set; }
+    internal string Color { get; set; }
+    internal DateTime LicenseCreationDate { private get; set; }
+    internal double Speed { get; set; } // km/h
+    internal decimal Price { get; set; }
+    public override string ToString()
+    {
+        return $" id:{Id}\n color: {Color}\n plate: {Plate}\n model: {Model}\n license expiration date: {DateOnly.FromDateTime(LicenseExpirationDate)}\n License validation: {IsLicenseValid}\n speed: {Speed}\n price: {Price}";
+    }
+}
---
+++ OOP.Task_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using OOP.Task_01;
+
+Car car = new Car();
+car.Id = 1;
+car.Letters = "asdc";
+car.Numbers = "123654";
+car.Color = "red";
+car.Model = "BMW";
+try
+{
+    car.LicenseCreationDate = new DateTime(2013, 11, 31);
+}
+catch (ArgumentOutOfRangeException argumentOutOfRangeException)
+{
+    car.LicenseCreationDate = DateTime.Now;
+    Console.WriteLine($" warning!! => license creation date is not valid date or month, day\n license creation date take {DateTime.Now}\n go to change it if you need.");
+    Console.WriteLine("========================================");
+    Console.Clear();
+}
+car.Speed = 400;
+car.Price = 15000.5M;
+Console.WriteLine(car.ToString());
